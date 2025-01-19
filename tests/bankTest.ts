88be55e436643f6eb903f94c7e5723823e0759ce
Ahmad El Idrissi Amiri <sidi_amiri@Ahmads-MacBook-Pro.local>
2025-01-18 21:21:33 -0500

    resolves #2 deposit money into an account

--- tests/bankTest.ts
+++ tests/bankTest.ts
@@ -12,8 +12,10 @@ const bank = new Bank(accounts, usernames);
 // Scenario 1: successful account creation
 const account = bank.createAccount("Alice", 20, 1234567895);
 if(account.id !== 1234567895 || account.balance !== 0 || account.id.toString().length !== 10){
+   
     console.log('Scenario 1 failed');
 }else{
+    console.log('Testing Account Creation');
     console.log('Scenario 1 passed');
 }
 try{
@@ -24,4 +26,31 @@ try{
     console.log('Scenario 2 passed');
 }   
 
-// Scenario 3: account number is invalid
+
+// Scenario 1: Successful Deposit
+try {
+    const message = bank.deposit(1234567890, 500);
+    console.log('Testing Deposit');
+    console.log('Scenario 1 passed:', message);
+} catch (e) {
+    console.log('Scenario 1 failed');
+}
+
+// Scenario 2: Unsuccessful Deposit due to Invalid Account
+try {
+    bank.deposit(9999999999, 500);
+    console.log('Scenario 2 failed');
+} catch (e) {
+    console.log('Scenario 2 passed');
+}
+
+// Scenario 3: Unsuccessful Deposit due to Invalid Amount
+try {
+    bank.deposit(1234567890, -500);
+    console.log('Scenario 3 failed');
+} catch (e) {
+    console.log('Scenario 3 passed');
+}
+
+
+
